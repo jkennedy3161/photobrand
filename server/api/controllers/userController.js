e7011e64e588695fc818a5ca7bebfa91b428bd67
jkennedy3161 <kennedyjt88@gmail.com>
2017-07-15 15:15:08 -0700

    add user controller methods

--- server/api/controllers/userController.js
+++ server/api/controllers/userController.js
@@ -1,5 +1,77 @@
+import _ from 'lodash';
+
+let User = require('../models/userModels');
+
+
+exports.params = function(req, res, next, id) {
+  User.findById(id)
+    .select('-password')
+    .exec()
+    .then(function(user) {
+      if (!user) {
+        next(new Error('No user with that id'));
+      } else {
+        req.user = user;
+        next();
+      }
+    }, function(err) {
+      next(err);
+    });
+};
+
 exports.get = function(req, res, next) {
-  res.json({
-    ok: true
+  User.find({})
+    .select('-password')
+    .exec()
+    .then(function(user) {
+      res.json(user);
+    }, function(err) {
+      next(err);
+    })
+};
+
+exports.getOne = function(req, res, next) {
+  var user = req.user;
+  res.json(user);
+};
+
+exports.put = function(req, res, next) {
+  var user = req.user;
+
+  var update = req.body;
+
+  _.merge(user, update);
+
+  user.save(function(err, saved) {
+    if (err) {
+      next(err);
+    } else {
+      res.json(saved.toJson());
+    }
+  })
+};
+
+exports.post = function(req, res, next) {
+  var newUser = new User(req.body);
+
+  newUser.save(function(err, user) {
+    if(err) { return next(err);}
+
+    var token = signToken(user._id);
+    res.json({token: token});
   });
+};
+
+exports.delete = function(req, res, next) {
+  req.user.remove(function(err, removed) {
+    if (err) {
+      next(err);
+    } else {
+      res.json(removed.toJson());
+    }
+  })
+};
+
+exports.me = function(req, res) {
+  res.json(req.user.toJson());
 };

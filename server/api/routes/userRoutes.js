771f1cb2a9aae5f57bbfccee4e9bc0651883d6d2
jkennedy3161 <kennedyjt88@gmail.com>
2017-07-15 15:18:40 -0700

    add remaining routes

--- server/api/routes/userRoutes.js
+++ server/api/routes/userRoutes.js
@@ -1,8 +1,18 @@
 var router = require('express').Router();
-
 var userController = require('../controllers/userController');
+var auth = require('../../auth/auth');
+var checkUser = [auth.decodeToken(), auth.getFreshUser()];
+
+router.param('id', userController.params);
+router.get('/me', checkUser, userController.me);
 
 router.route('/')
-  .get(userController.get);
+  .get(userController.get)
+  .post(userController.post);
+
+router.route('/:id')
+  .get(userController.getOne)
+  .put(checkUser, userController.put)
+  .delete(checkUser, userController.delete);
 
 module.exports = router;

1a33e96f9039a31cbe53337ce072dba73e348142
jkennedy3161 <kennedyjt88@gmail.com>
2017-07-15 14:44:08 -0700

    add users route test

--- test/index.js
+++ test/index.js
@@ -7,8 +7,15 @@ import assert from  'assert';
 describe('Example Node Server', () => {
   it('should return 200', done => {
     app.listen(8080);
-    app.get('http://127.0.0.1:8080', req => {
+    app.get('http://127.0.0.1:8080', res => {
       assert.equal(200, res.statusCode);
+    });
+    done();
+  });
+  it('should return true with /api/users route', done => {
+    app.listen(8080);
+    app.get('http://127.0.0.1:8080/api/users', res => {
+      assert.equal(true, res.json.ok);
     });
     done();
   });

b873adf608d9de9212de0647b8230a1fe40b972d
jkennedy3161 <kennedyjt88@gmail.com>
2017-07-16 09:45:27 -0700

    add main file for injecting to other scripts on frontend

---
+++ client/app/app.js
@@ -0,0 +1,24 @@
+angular.module('photobrand', [
+  'photobrand.account',
+  'photobrand.signup',
+  'photobrand.profile',
+  'photobrand.services',
+  'ngRoute'])
+.config(function($routeProvider) {
+  $routeProvider
+    .when('/', {
+      templateUrl: 'app/account/account.html',
+      controller: 'AccountController'
+    })
+    .when('/signup', {
+      templateUrl: 'app/signup/signup.html',
+      controller: 'SignUpController'
+    })
+    .when('/profile', {
+      templateUrl: 'app/profile/profile.html',
+      controller: 'ProfileController'
+    })
+    .otherwise('/', {
+      templateUrl: 'app/account/account.html'
+    })
+});

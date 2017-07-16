e5a4bf6411065ff1e6d0e0dd310e76c475eec1e8
jkennedy3161 <kennedyjt88@gmail.com>
2017-07-16 16:12:09 -0700

    add logic for displaying logged in username

--- client/app/profile/profile.js
+++ client/app/profile/profile.js
@@ -1,4 +1,12 @@
 angular.module('photobrand.profile', [])
-.controller('ProfileController', function($scope) {
-  $scope.test = 'load your avatar here and set up your profile';
+.controller('ProfileController', function($scope, Auth, $window) {
+  $scope.token = $window.localStorage.getItem('access_token');
+  $scope.user = {};
+  $scope.userId = function() {
+    Auth.me($scope.token)
+      .then(function(user) {
+        $scope.user = user;
+        $window.localStorage.setItem('userId', user._id);
+      })
+  };
 });

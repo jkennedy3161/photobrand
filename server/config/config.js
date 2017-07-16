6505e971dd5add543451872c8f2bbaaaea15158e
jkennedy3161 <kennedyjt88@gmail.com>
2017-07-15 20:57:03 -0700

    add secret config with expireTime

--- server/config/config.js
+++ server/config/config.js
@@ -2,11 +2,16 @@
 import _ from 'lodash';
 
 // import internal scripts
+let jwt = require('../resources/secret');
 
 // set config object
 let config = {
   port: process.env.PORT || 8080,
-  db: {url: 'mongodb://127.0.0.1:27017/photobrand'}
+  db: {url: 'mongodb://127.0.0.1:27017/photobrand'},
+  expireTime: '10d',
+  secrets: {
+    jwt: process.env.JWT || jwt.secret
+  }
 };
 
 module.exports = config;

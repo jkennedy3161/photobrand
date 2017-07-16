6fde8f7345b466b875abb7af51cd0b58d70c9122
jkennedy3161 <kennedyjt88@gmail.com>
2017-07-16 16:09:33 -0700

    add logic for signup

--- client/app/signup/signup.js
+++ client/app/signup/signup.js
@@ -1,4 +1,21 @@
 angular.module('photobrand.signup', [])
-.controller("SignUpController", function($scope) {
-  $scope.test = 'register here';
+.controller('SignUpController', function($scope, Auth, $window, $location) {
+  $scope.modal = function() {
+    $('.modal-backdrop').hide();
+    $('#authenticate').modal('show');
+  };
+  $scope.signup = function(username, password) {
+    let data = {
+      username: username,
+      password: password
+    };
+    Auth.signup(data)
+      .then(function(token) {
+        $window.localStorage.setItem('access_token', token);
+        $('#authenticate.modal').hide();
+        $('.modal-backdrop').hide();
+        $location.path('/profile');
+      })
+      .catch(function(err) {});
+  };
 });

a88a085b376ee2b1c8838e9c26e1fb2faa577100
jkennedy3161 <kennedyjt88@gmail.com>
2017-07-17 16:31:14 -0700

    add avatar file select with image crop tool

--- client/app/profile/profile.js
+++ client/app/profile/profile.js
@@ -1,12 +1,33 @@
 angular.module('photobrand.profile', [])
-.controller('ProfileController', function($scope, Auth, $window) {
+.controller('ProfileController', function($scope, Auth, $window, Upload, $timeout) {
   $scope.token = $window.localStorage.getItem('access_token');
   $scope.user = {};
+  $scope.myImage='';
+  $scope.myCroppedImage = '';
   $scope.userId = function() {
+    $('.modal-backdrop').hide();
+    $('#profile').modal('show');
     Auth.me($scope.token)
       .then(function(user) {
         $scope.user = user;
         $window.localStorage.setItem('userId', user._id);
       })
   };
+
+  $scope.uploadPic = function(file) {
+    // upload image to server
+  };
+    var handleFileSelect = function(evt) {
+      var file = evt.currentTarget.files[0];
+      //console.log(file.target.files[0]);
+      var reader = new FileReader();
+      reader.onload = function(evt) {
+        $scope.$apply(function($scope) {
+          $scope.myImage = $scope.myCroppedImage = evt.target.result;
+        });
+      };
+      reader.readAsDataURL(file);
+    };
+
+    angular.element(document.querySelector('#fileInput')).on('change',handleFileSelect);
 });

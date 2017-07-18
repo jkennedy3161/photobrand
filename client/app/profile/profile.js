888158b675e4b137b814aa1ead32ddd335a65006
jkennedy3161 <kennedyjt88@gmail.com>
2017-07-18 10:04:41 -0700

    add logic and layout for profile with avatar setup

--- client/app/profile/profile.js
+++ client/app/profile/profile.js
@@ -1,25 +1,50 @@
 angular.module('photobrand.profile', [])
-.controller('ProfileController', function($scope, Auth, $window, Upload, $timeout) {
+.controller('ProfileController', function($scope, Auth, $window, Profile, $location) {
   $scope.token = $window.localStorage.getItem('access_token');
   $scope.user = {};
   $scope.myImage='';
   $scope.myCroppedImage = '';
+  $scope.myProfile;
   $scope.userId = function() {
-    $('.modal-backdrop').hide();
-    $('#profile').modal('show');
     Auth.me($scope.token)
       .then(function(user) {
         $scope.user = user;
         $window.localStorage.setItem('userId', user._id);
       })
+      .then(function() {
+        Profile.getProfile()
+          .then(function(userProfile) {
+            console.log(userProfile);
+            if(userProfile) {
+              $scope.myProfile = userProfile;
+              $('.modal-backdrop').hide();
+              $('#profile').modal('hide');
+            } else {
+              $('.modal-backdrop').hide();
+              $('#profile').modal('show');
+            }
+          });
+      });
   };
 
-  $scope.uploadPic = function(file) {
-    // upload image to server
+  $scope.uploadProfile = function(firstName, lastName, email, studioName) {
+    let profile = {
+      firstName: firstName,
+      lastName: lastName,
+      email: email,
+      avatar: $scope.myCroppedImage,
+      studioName: studioName,
+      account: $window.localStorage.getItem('userId')
+    };
+    Profile.saveProfile(profile)
+      .then(function(result) {
+        console.log(result);
+        $scope.myProfile = result;
+        $('#profile').modal('hide');
+      });
   };
     var handleFileSelect = function(evt) {
       var file = evt.currentTarget.files[0];
-      //console.log(file.target.files[0]);
       var reader = new FileReader();
       reader.onload = function(evt) {
         $scope.$apply(function($scope) {
@@ -30,4 +55,9 @@ angular.module('photobrand.profile', [])
     };
 
     angular.element(document.querySelector('#fileInput')).on('change',handleFileSelect);
+
+    $scope.signout = function() {
+      $window.localStorage.clear();
+      $location.path('/');
+    };
 });
